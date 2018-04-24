e4201f534ec5c1e1033220218629c811244274a5
Lucas Marioza <lucasmarioza@gmail.com>
2018-04-24 11:33:40 -0300

    first

--- index.js
+++ index.js
@@ -2,26 +2,10 @@ const Slimbot = require("slimbot")
 const slimbot = new Slimbot(process.env["BOT_KEY"])
 const restify = require("restify")
 
-const commands = require("./commands")
-const handleMessage = require("./handlers")
-
-const firebase = require("firebase")
 
 let server = restify.createServer()
 //console.log(restify);
 server.use(restify.plugins.bodyParser())
-
-// Initialize Firebase
-var config = {
-  apiKey: process.env["FIREBASE_KEY"],
-  authDomain: "mentionbot-fa86b.firebaseapp.com",
-  databaseURL: "https://mentionbot-fa86b.firebaseio.com",
-  projectId: "mentionbot-fa86b",
-  storageBucket: "mentionbot-fa86b.appspot.com",
-  messagingSenderId: "271181642792"
-}
-
-const fireApp = firebase.initializeApp(config)
 
 slimbot.setWebhook({
   url: `${process.env["ORIGIN"]}/${process.env["BOT_KEY"]}`
@@ -37,18 +21,7 @@ server.post(`/${process.env["BOT_KEY"]}`, function handle(req, res, next) {
     return next()
   }
 
-  mention = firebase
-    .database()
-    .ref(`/groups/${message.chat.id}`)
-    .once("value")
-    .then(function(snapshot) {
-      commands.setMentionsVar(snapshot.val() || {})
-      handleMessage(message)
-      firebase
-        .database()
-        .ref(`/groups/${message.chat.id}`)
-        .set(commands.getMentionsVar())
-    })
+  slimbot.sendMessage(message.chat.id,message.text)
   res.send(200)
   return next()
 })
--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
-# Telegram Group Mention
-Create custom mentions in your Telegram chat
+# Bino Bot
+Let your message be a Bino message
 
 ## Getting Started
 Clone this repo
 ```bash
-git clone https://github.com/lucasMarioza/TelegramGroupMention
+git clone https://github.com/lucasMarioza/BinoBot
 ```
 Install dependencies
 ```bash
@@ -13,11 +13,5 @@ npm install
 Set environment variables and run
 ```bash
 export BOT_KEY=...
-export FIREBASE_KEY=...
 npm start
 ```
-
-## Commands
-`/newMention MENTION_NAME` creates a new group mention  
-`/assignTo MENTION_NAME` subscribe to a group mention  
-`/deleteMention MENTION_NAME` deletes a group mention
